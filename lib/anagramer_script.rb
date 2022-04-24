edb80303a13bd5dc01687051abfd3c86cb0d2416
Aaron Boyd <a.boyd91@yahoo.com>
2022-04-24 00:01:04 -0700

    add script for the user to use Anagramer

--- lib/anagramer_script.rb
+++ lib/anagramer_script.rb
@@ -1,5 +1,45 @@
 #!/usr/bin/ruby
 require_relative 'anagramer'
 
-#Create a script that asks user to input 2 words or phrases to see if they are anagrams
+puts "Hello!"
+puts "Welcome to The Anagramer!"
+puts "Would you like to use the Anagramer? y or n"
+play = gets.chomp()
+if play == "y"  
+  puts "Did you know that an Anagram is when two words or phrases are different but contain all the same letters?" 
+  puts "Also, an Antigram is when two words or phrases contain none of the same letters."
+  puts "Please enter the first word or phrase."
+  phrase1 = gets.chomp()
+  puts "Please enter another word or phase to see if it is an Anagram of the second word."
+  phrase2 = gets.chomp()
+  puts "Please hold....we are processing."
+  puts '.'
+  puts '.'
+  puts '.'
+  puts '.'
+  puts '.'
+  puts '.................still proccessing'
+  puts '.'
+  puts '.'
+  puts '.'
+  puts '.'
+  puts '.'
+  puts '.'
+  puts '.'
+  puts '.'
+  puts '.'
+  puts '.'
+
+  puts 'The result of your input....'
+  gram = Anagram.new("#{phrase1}","#{phrase2}" )
+  puts gram.anagram_checker
+else
+  puts "Well, fine then."
+end
+
+
+
+
+
+
 
